b5c1250ac3fbd46f4f6dee1b282892d6171095c5
Sylvain Boissel <sylvain@ashtree.eu>
2016-06-23 18:17:54 +0200

    admin view

--- booksplitter/models.py
+++ booksplitter/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
 class WD_Property(models.Model):
     qid = models.CharField(max_length=12)
-    category = models.CharField(max_length=40)
+    category = models.ForeignKey(Category)
 
     def __str__(self):
         return self.qid
